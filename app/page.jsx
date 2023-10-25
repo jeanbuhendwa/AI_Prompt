bb7b5c32b5aa8c3fe220b2d16574a582f1e4776a
jeanbuhendwa <jeanjacquesirenge40@gmail.com>
2023-10-25 22:34:19 +0300

    Add home page

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,4 @@
+import Feed from "@components/Feed";
 const Home = () => {
   return (
     <section className="w-full flex-center flex-col">
@@ -10,6 +11,7 @@ const Home = () => {
         Prompt AI is an open-source AI prompting tool for modern world to
         discover, create and share creative prompts
       </p>
+      <Feed />
     </section>
   );
 };
