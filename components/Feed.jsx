0234117c23b7d88ea51a7a3a51edcfb42640c94d
jeanbuhendwa <jeanjacquesirenge40@gmail.com>
2023-11-01 20:39:48 +0300

    Update feed

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,7 +1,29 @@
-import React from "react";
+"use client";
 
+import { useState, useEffect } from "react";
+import PromptCard from "./PromptCard";
+
+const PromptCardList = ({ data, handleTagClick }) => {
+  return <div className="mt-16 prompt_layout"></div>;
+};
 const Feed = () => {
-  return <div>Feed</div>;
+  const [searchText, setSearchText] = useState("");
+  const handleSearchChange = (e) => {};
+  return (
+    <section className="feed ">
+      <form className="relative w-full flex-center">
+        <input
+          type="text"
+          placeholder="Search for a tag or Username"
+          value={searchText}
+          onChange={handleSearchChange}
+          required
+          className="search_input peer"
+        />
+      </form>
+      <PromptCardList data={[]} handleTagClick={() => {}} />
+    </section>
+  );
 };
 
 export default Feed;
